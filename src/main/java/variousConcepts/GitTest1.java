37a17380211ba54e8646c644c900695ebb0c73ee
shihabamin <amin.khandaker@gmail.com>
2021-11-21 10:11:21 -0600

    message

--- src/main/java/variousConcepts/GitTest1.java
+++ src/main/java/variousConcepts/GitTest1.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("git test1");
 		System.out.println("git test2");
+		System.out.println("Modification dones");
 		System.out.println("git test3");
 	}
 
